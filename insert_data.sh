70109f9a388767f901f11c6a1979f661ae041fcb
Joy Susette  Domingo <joysusette@gmail.com>
2024-12-25 03:24:03 +0000

    Save progress: 1.1

--- insert_data.sh
+++ insert_data.sh
@@ -1,5 +1,6 @@
-#! /bin/bash
+#!/bin/bash
 
+# Use psql command for test or production databases
 if [[ $1 == "test" ]]
 then
   PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
@@ -7,4 +8,17 @@ else
   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
 fi
 
-# Do not change code above this line. Use the PSQL variable above to query your database.
+# Read the games.csv file line by line
+while IFS=',' read -r year round winner opponent winner_goals opponent_goals
+do
+  # Skip the header line
+  if [[ "$year" != "year" ]]; then
+    # Insert both winner and opponent into the teams table (ignore duplicates)
+    $PSQL "INSERT INTO teams (name) VALUES ('$winner') ON CONFLICT (name) DO NOTHING"
+    $PSQL "INSERT INTO teams (name) VALUES ('$opponent') ON CONFLICT (name) DO NOTHING"
+
+    $PSQL "INSERT INTO games (year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES ($year, '$round', (SELECT team_id FROM teams WHERE name = '$winner'), (SELECT team_id FROM teams WHERE name = '$opponent'),$winner_goals, $opponent_goals)"
+  fi
+done < games.csv
+
+echo "Teams have been inserted into the table from games.csv."
